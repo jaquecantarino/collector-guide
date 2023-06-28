eb343898d0304228408fdb8b30f5b8331781ef75
Jaqueline Cantarino <jaquelinecantarino@outlook.com>
2023-06-28 20:59:42 -0300

    primeiro commit: inicializando projeto

---
+++ kcollectorguide/src/main/java/com/kcollectorguide/kcollectorguide/KcollectorguideApplication.java
@@ -0,0 +1,13 @@
+package com.kcollectorguide.kcollectorguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcollectorguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcollectorguideApplication.class, args);
+	}
+
+}
